35acd537805b04d70fcded409b137df703694f61
AnushkaSoni <anushkasoni035@gmail.com>
2021-04-03 13:40:09 +0530

    Commented temp file

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from datetime import date
 from flask_session import Session
-from tempfile  import mkdtemp
+#from tempfile  import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
